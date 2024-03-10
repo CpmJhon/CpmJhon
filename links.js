795ecf70d8c3563ad626a2cafaad3ab63a5a0ef6
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2024-03-10 20:54:34 +0700

    Add files via upload

---
+++ links.js
@@ -0,0 +1,37 @@
+export const links = [
+    {
+        name: "WhatsApp",
+        link: 
+"https://wa.me/6285894955362",
+        image: 
+"whatapps.png",
+    },
+    {
+        name: "YouTube",
+        link: 
+"https://youtube.com/@galeri_car_cpm8048",
+        image: 
+"youtube.png",
+    },
+    {
+        name: "Instagram",
+        link: 
+"https://instagram.com/cpm_jhon?igshid=YmMyMTA2M2Y=",
+        image: 
+"instagram.png",
+    },
+    {
+        name: "TikTok",
+        link: 
+"https://tiktok.com/@garasidriftcpm",
+        image: 
+"tiktok.png",
+    },
+    {
+        name: "Website",
+        link: 
+"https://shorturl.at/dkCO0",
+        image: 
+"globe.png",
+    },
+];
