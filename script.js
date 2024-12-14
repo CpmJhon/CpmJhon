04094027360bc7ae8ad52d1ed7d52f72729f2769
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2024-12-15 06:35:19 +0700

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         window.location.href = "index2.html";
-    }, 25000); // 5 detik
+    }, 4000); // 5 detik
 });
 
 // Inisialisasi Particles.js
